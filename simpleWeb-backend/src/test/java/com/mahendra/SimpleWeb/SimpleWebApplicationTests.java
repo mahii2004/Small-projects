ac22b8660f6adcf2a32d0ca0a6e5cbcc448e7a15
Mahendra Dadibathina <mahendradadibathina@gmail.com>
2025-06-23 18:47:43 +0530

    Simple CRUD

---
+++ simpleWeb-backend/src/test/java/com/mahendra/SimpleWeb/SimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahendra.SimpleWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
